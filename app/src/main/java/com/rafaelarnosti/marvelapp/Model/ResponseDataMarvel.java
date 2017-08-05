c83a9a6291078cc391131d42cda9e5b7a93257fd
Rafael Arnosti <rlm.arnosti@gmail.com>
2017-08-05 17:21:38 -0300

    Implementado novas classes

---
+++ app/src/main/java/com/rafaelarnosti/marvelapp/Model/ResponseDataMarvel.java
@@ -0,0 +1,20 @@
+package com.rafaelarnosti.marvelapp.Model;
+
+import java.util.List;
+
+/**
+ * Created by logonrm on 05/08/2017.
+ */
+
+public class ResponseDataMarvel {
+
+    List<Super> results;
+
+    public List<Super> getResults() {
+        return results;
+    }
+
+    public void setResults(List<Super> results) {
+        this.results = results;
+    }
+}
---
+++ app/src/main/java/com/rafaelarnosti/marvelapp/Model/ResponseHeroiMarvel.java
@@ -0,0 +1,18 @@
+package com.rafaelarnosti.marvelapp.Model;
+
+/**
+ * Created by logonrm on 05/08/2017.
+ */
+
+public class ResponseHeroiMarvel {
+
+    ResponseDataMarvel data;
+
+    public ResponseDataMarvel getData() {
+        return data;
+    }
+
+    public void setData(ResponseDataMarvel data) {
+        this.data = data;
+    }
+}
